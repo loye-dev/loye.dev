7e021c225195ee216bce24918b27323e8aeecce1
Tristan Loye <73702803+tloye@users.noreply.github.com>
2026-04-21 00:05:26 +0200

    feat: add github link button

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,4 +2,12 @@
   <div class="flex min-h-screen items-center justify-center">
     <LogoAnimation />
   </div>
+  <UButton
+    to="https://github.com/loye-dev/loye.dev"
+    target="_blank"
+    icon="simple-icons:github"
+    variant="ghost"
+    color="neutral"
+    class="fixed bottom-4 right-4 opacity-30 hover:opacity-70"
+  />
 </template>
